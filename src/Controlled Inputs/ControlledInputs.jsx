545a7dc497ced514b6bb0ae91f4babf2566a0362
zaheerahmadz <zaheerahmedjudg@gmail.com>
2025-12-21 07:23:24 -0700

    controlled inputs

--- src/Controlled Inputs/ControlledInputs.jsx
+++ src/Controlled Inputs/ControlledInputs.jsx
@@ -5,13 +5,19 @@ const ControlledInputs = () => {
   const [email, setEmail] = useState("");
   const [message, setMessage] = useState("");
   const [people, setPeople] = useState([]);
+
+  // Delete person by index
+  const Delete = (indexToDelete) => {
+    setPeople((people) => {
+      return people.filter((_, index) => index !== indexToDelete);
+    });
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (firstName && email && message) {
       const person = { firstName, email, message };
-      setPeople((people) => {
-        return [...people, person];
-      });
+      setPeople((people) => [...people, person]);
       setFirstName("");
       setMessage("");
       setEmail("");
@@ -23,7 +29,10 @@ const ControlledInputs = () => {
   return (
     <>
       <article>
-        <form className="max-w-md mx-auto mt-10 bg-white p-6 rounded-2xl shadow-md space-y-5">
+        <form
+          onSubmit={handleSubmit}
+          className="max-w-md mx-auto mt-10 bg-white p-6 rounded-2xl shadow-md space-y-5"
+        >
           <h2 className="text-2xl font-semibold text-center text-gray-800">
             Application Form
           </h2>
@@ -74,7 +83,6 @@ const ControlledInputs = () => {
           </div>
 
           <button
-            onClick={handleSubmit}
             type="submit"
             className="w-full bg-blue-600 text-white py-2 rounded-lg font-medium hover:bg-blue-700 transition duration-300"
           >
@@ -82,7 +90,8 @@ const ControlledInputs = () => {
           </button>
         </form>
       </article>
-      <div className=" container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8  my-5  ">
+
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-8 my-5">
         {people.map((person, index) => {
           const { firstName, email, message } = person;
           return (
@@ -95,12 +104,19 @@ const ControlledInputs = () => {
               </h3>
 
               <p className="text-sm text-blue-600 mb-3 break-all">
-                Email:{email}
+                Email: {email}
               </p>
 
               <p className="text-gray-700 text-sm leading-relaxed">
-                Message:{message}
+                Message: {message}
               </p>
+
+              <button
+                onClick={() => Delete(index)}
+                className="my-5 cursor-pointer hover:bg-red-600 bg-red-500 px-5 py-3 text-white rounded-md"
+              >
+                Delete
+              </button>
             </div>
           );
         })}
